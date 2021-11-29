9da74196e6be83c37c39b9e7130e36b0c071b805
amirhamzatanha <amirhamza.tanha@gmail.com>
2021-11-28 20:42:28 -0500

    Final Version

--- control/handlePages.js
+++ control/handlePages.js
@@ -2,6 +2,7 @@ const viewUsers = require('../services/viewUsers');
 const inventory = require('../services/inventory');
 const { body } = require('express-validator');
 const delivery = require('../services/delivery');
+const editItem = require('../services/editItem');
 
 const handleIndex = async (req, res) => {
     return res.render('index.ejs',{
@@ -9,6 +10,31 @@ const handleIndex = async (req, res) => {
     });
 };
 
+
+
+const transactionDetails = async( req,res) => {
+    const userid = req.user.employeeID;
+    console.log('userID in Transaction Details ' + userid);
+
+    try{
+        const transcDetails = await inventory.viewTransactions(userid);
+        console.log(transcDetails);
+        const total = await inventory.viewTotalTransactions(userid);
+        const result = total[0].sumTranc + total[0].tax
+        console.log(total.sum);
+        return res.render('cashierDetails', { user: req.user, data: transcDetails, result: result.toFixed(2),  errors: req.flash("errors")});
+
+    }
+    catch(err){
+        req.flash("errors", err);
+        console.log(err);
+        res.redirect("/");
+
+    }
+    
+
+    
+};
 
 
 const handleViewUsers = async( req,res) => {
@@ -70,8 +96,8 @@ const addInventoryGit = async( req,res) => {
 
     try{
         const departments = await inventory.viewDepartments();
-        const data = [];
-        return res.render('editInventory', { user: req.user, dept: departments, data: data, errors: req.flash("errors")});
+        const message =[];
+        return res.render('editInventory', { user: req.user, dept: departments, message, errors: req.flash("errors")});
 
     }
     catch(err){
@@ -83,6 +109,31 @@ const addInventoryGit = async( req,res) => {
     
 };
 
+const addInventoryPost = async( req,res) => {
+    const data = {
+        UPC: req.body.UPC,
+        deptNum: req.body.deptNum,
+        price: req.body.price,
+        itemDescription: req.body.itemDescription,
+        notes: req.body.notes,
+        amount: req.body.amount
+    }
+    console.log ("Data Received ============" + data);
+
+    try{
+        const message = await inventory.addNewItem(data);
+        const departments = await inventory.viewDepartments();
+        return res.render('editInventory', { user: req.user, dept: departments, message, errors: req.flash("errors")});
+
+    }
+    catch(err){
+        req.flash("errors", err);
+        console.log(err);
+        res.redirect("/addInventory");
+
+    }
+    
+};
 
 const updateUser = async( req,res) => {
     const employeeID = req.body.employeeID;
@@ -110,11 +161,10 @@ const updateUser = async( req,res) => {
     
 };
 
-
 const addDeliveryPage = async (req, res) => {
     console.log("Delivering Delivery Page");
     return res.render('delivery.ejs',{
-        user: req.user
+        user: req.user, errors: req.flash("errors")
     });
 };
 
@@ -128,25 +178,70 @@ const addDelivery = async( req,res) => {
         };
 
         amountInt = parseInt(updateData.Amount);
-        console.log(updateData);
-        console.log(typeof(req.body.UPC));
-        console.log(typeof(amountInt));
-        console.log("Made it to the addDelivery Function");
+        
         const itemAmount = await delivery.findItemExistingAmount(updateData.UPC);
-        console.log("Past Function");
-        console.log(itemAmount);
-        console.log(typeof(itemAmount));
+       
         const newTotal = await delivery.updateItemInventory(itemAmount, amountInt, updateData.UPC);
         console.log("DONE WITH FUNCTION!")
         return res.render('delivery.ejs', { user: req.user, newTotal: newTotal, errors: req.flash("errors") } );
     }
-    catch{
-        console.log("Error Function");
-        //req.flash("errors", err);
-        return res.send('Error! The UPC you entered does not exist in the system. Have the Price Coordinator enter a new item. Use the back button to proceed.');
+    catch (err){
+        req.flash("errors", err);
+        
+        return res.redirect('/delivery');
+        //return res.send('Error! The UPC you entered does not exist in the system. Have the Price Coordinator enter a new item. Use the back button to proceed.');
 
     }
     
+};
+
+
+const handleShowItem = async (req,res) => {
+    try {
+        const itemInfo = await editItem.showItem(req.params.UPC);
+        res.render('editItem',{user:req.user, itemInfo, errors:req.flash("error")});
+
+    }
+    catch (err) {
+        req.flash("errors", err);
+        res.redirect("/inventory");
+    }
+}
+
+const updateItem = async( req,res) => {
+    
+    const data1 = 
+    {
+        itemDescription: req.body.itemDescription,
+        price:  req.body.price,
+        notes: req.body.notes
+    }
+
+    try{
+        await editItem.updateItem(data1, req.body.UPC)
+        const departments = await inventory.viewDepartments();
+        const data = [];
+        return res.render('viewInventory', { user: req.user, dept: departments, data: data, errors: req.flash("errors")})
+    }
+    catch (err){
+        req.flash("errors", err);
+        res.redirect("viewInventory");
+
+    }
+    
+};
+
+const handleDeleteItem = async(req,res) => {
+    try{
+        await editItem.deleteItem(req.params.UPC);
+        const departments = await inventory.viewDepartments();
+        const data = [];
+        return res.render('viewInventory', { user: req.user, dept: departments, data: data, errors: req.flash("errors")});
+    }
+    catch{
+        req.flash('errors',err);
+        res.redirect("/");
+    }
 };
 
 module.exports = {
@@ -159,5 +254,10 @@ module.exports = {
     addInventoryGit: addInventoryGit,
     updateUser: updateUser,
     addDeliveryPage: addDeliveryPage,
-    addDelivery: addDelivery
+    addDelivery: addDelivery,
+    handleShowItem: handleShowItem,
+    updateItem: updateItem,
+    handleDeleteItem: handleDeleteItem,
+    addInventoryPost: addInventoryPost,
+    transactionDetails: transactionDetails
 };
